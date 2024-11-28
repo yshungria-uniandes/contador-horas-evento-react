c17b730b5e5cdd985d8edec4e97e6eaeb7390256
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-11-27 22:43:56 -0500

    Components created

---
+++ src/components/EventCountDown.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+
+const EventCountdown = ({ targetDate }) => {
+  const calculateTimeLeft = () => {
+    const difference = new Date(targetDate) - new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [targetDate]);
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span key={interval}>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Countdown to Event</h1>
+      {timerComponents.length ? timerComponents : <span>Event has started!</span>}
+    </div>
+  );
+};
+
+export default EventCountdown;
